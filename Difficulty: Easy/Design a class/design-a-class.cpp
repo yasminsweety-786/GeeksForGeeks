b4455f27a1f1c74a6997b8527ef6cf95fe264c62
yasmin_shaik <yasminsweety410@gmail.com>
2026-06-29 08:09:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Design a class/design-a-class.cpp
@@ -0,0 +1,11 @@
+// Design a class as described in the question
+// code here
+#include <iostream>
+using namespace std;
+
+class MyClass {
+public:
+    void display() {
+        cout << "Hello World" << endl;
+    }
+};
