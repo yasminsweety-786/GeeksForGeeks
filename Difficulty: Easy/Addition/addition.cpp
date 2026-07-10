de53d748edb2d18fe34acb2afaf96a3dd7f549bc
yasmin_shaik <yasminsweety410@gmail.com>
2026-07-10 08:54:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Addition/addition.cpp
@@ -0,0 +1,14 @@
+// create a class addition
+// with a function add
+// this should return the sum of
+// two values(a,b) passed in the parameter
+
+// code here
+class Addition{
+    public:
+           int add(int a,int b)
+           {
+              return a+b;
+           }
+    
+};
