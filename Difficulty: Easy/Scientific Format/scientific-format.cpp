eb04c56dd1ec903077d53e0925452ab1ea5d3668
yasmin_shaik <yasminsweety410@gmail.com>
2026-07-12 09:32:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Scientific Format/scientific-format.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // code here
+    double a; 
+    cin>>a;
+     cout<<scientific<<setprecision(4)<<a<<endl;
+     
+    
+
+    return 0;
+}
