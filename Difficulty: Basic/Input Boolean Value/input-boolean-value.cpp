143d53979a573ef8223f0700b9bf6518ba9b639b
yasmin_shaik <yasminsweety410@gmail.com>
2026-07-13 07:06:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Input Boolean Value/input-boolean-value.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+
+    // code here
+    cin>>s;
+    bool a = (s=="true");
+    
+
+    // Printing numeric representation of boolean
+    cout << a<<endl;
+
+    return 0;
+}
