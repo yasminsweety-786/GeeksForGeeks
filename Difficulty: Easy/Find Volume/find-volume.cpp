781d928ba22d59989a4c2a890d8ba293e13daabe
yasmin_shaik <yasminsweety410@gmail.com>
2026-07-20 08:53:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Volume/find-volume.cpp
@@ -0,0 +1,16 @@
+// Write the first function to
+// return the volume of cube
+int volume(int s) {
+    // code here
+    return s*s*s;
+    
+}
+
+// Write the second function to
+// return the volume of the rectangular box
+int volume(int l, int w, int h) {
+    
+    // code here
+     return l*w*h;
+}
+
