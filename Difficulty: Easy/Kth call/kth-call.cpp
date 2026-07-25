29a64555c459651ec38bc33b5019fdebf2750d77
yasmin_shaik <yasminsweety410@gmail.com>
2026-07-25 08:23:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Kth call/kth-call.cpp
@@ -0,0 +1,10 @@
+void utility() {
+
+    // declare the variable count here
+static int count = 0;
+    // increases the value of count by 1
+    count++;
+
+    // print count
+    cout << count;
+}
