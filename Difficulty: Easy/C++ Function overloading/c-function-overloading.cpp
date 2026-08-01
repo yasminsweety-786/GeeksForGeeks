d9b3a0ef3532279639c86957566db8aa62018b24
yasmin_shaik <yasminsweety410@gmail.com>
2026-08-01 10:47:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/C++ Function overloading/c-function-overloading.cpp
@@ -0,0 +1,28 @@
+void volume(int s)
+
+{
+
+    cout<<(s*s*s)<<endl;
+
+}
+
+ 
+
+//Function to calculate the volume of a cylinder
+
+void volume(int r, int h)
+
+{
+
+    cout<<(3.14159*r*r*h)<<endl;
+
+}
+//Function to calculate the volume of a rectangular cuboid
+
+void volume(int l, int b, int h)
+
+{
+
+    cout<<(l*b*h)<<endl;
+
+}
