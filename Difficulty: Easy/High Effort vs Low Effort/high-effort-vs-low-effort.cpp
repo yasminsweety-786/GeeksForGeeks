ac72803cb6474493ae5f7b345dd6af5aad0e0d8a
yasmin_shaik <yasminsweety410@gmail.com>
2026-08-10 08:55:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/High Effort vs Low Effort/high-effort-vs-low-effort.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int maxTask(vector<int>& h, vector<int>& l) {
+        // code here
+        int prev1 =0 ,prev2 = 0;
+        for(int i = 0 ;i<h.size() ; i++){
+            int curr = max(h[i] + prev2 , l[i] + prev1);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
